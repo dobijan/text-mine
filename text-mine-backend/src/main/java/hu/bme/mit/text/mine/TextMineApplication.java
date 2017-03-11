4880a36961e16d10c71dc95b7ddfbd3fbf247269
Jasek <dobi.jan.sandor@gmail.com>
2017-03-11 14:58:53 +0100

    Backend Spring project skeleton.

---
+++ text-mine-backend/src/main/java/hu/bme/mit/text/mine/TextMineApplication.java
@@ -0,0 +1,12 @@
+package hu.bme.mit.text.mine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextMineApplication.class, args);
+	}
+}
---
+++ text-mine-backend/src/test/java/hu/bme/mit/text/mine/TextMineApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.bme.mit.text.mine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextMineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
