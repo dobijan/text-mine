24aa3df788589af412cf605b0e1f9bd2fe7cb29e
Dobi Jan Sándor <dobi.jan.sandor@gmail.com>
2017-03-16 10:19:56 +0100

    MongoDB connectivity skeleton.

--- text-mine-backend/src/main/java/hu/bme/mit/textmine/TextMineApplication.java
+++ text-mine-backend/src/main/java/hu/bme/mit/textmine/TextMineApplication.java
@@ -1,9 +1,11 @@
-package hu.bme.mit.text.mine;
+package hu.bme.mit.textmine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath*:configuration/spring.application.context.xml"})
 public class TextMineApplication {
 
 	public static void main(String[] args) {
---
+++ text-mine-backend/src/main/java/hu/bme/mit/textmine/mongo/core/BaseMongoEntity.java
@@ -0,0 +1,39 @@
+package hu.bme.mit.textmine.mongo.core;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseMongoEntity implements Serializable {
+	private static final long serialVersionUID = 7035851559619057861L;
+
+	@Id
+	protected ObjectId id;
+
+	@Version
+	protected Long version;
+
+	@CreatedDate
+	protected Date createdDate;
+
+	@LastModifiedDate
+	protected Date lastModifiedDate;
+
+	@CreatedBy
+	protected String createdBy;
+
+	@LastModifiedBy
+	protected String lastModifiedBy;
+}
